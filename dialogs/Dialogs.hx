35fc664cfb892f8a948382e16d8410dbe933b70d
underscorediscovery <sven@underscorediscovery.com>
2015-08-19 02:54:24 -0230

    Undo tests and add missing vector include for windows

--- dialogs/Dialogs.hx
+++ dialogs/Dialogs.hx
@@ -1,47 +1,12 @@
 package dialogs;
 
-import cpp.vm.Thread;
-
 
 @:keep
+@:include('linc_dialogs.h')
 @:build(linc.Touch.apply())
 @:buildXml("<include name='${haxelib:linc_dialogs}/linc/linc_dialogs.xml'/>")
-class Dialogs {
+extern class Dialogs {
 
-    public static function open(title:String, ?filters:Array<FileFilter>) : String {
-
-        #if mac
-            return DialogsLinc.open(title, filters);
-        #else
-
-            var primary = Thread.current();
-                //make a copy
-            var tf = (filters != null) ? filters.copy() : null;
-            var of = ''+title;
-
-            Thread.create(function(){
-                primary.sendMessage(DialogsLinc.open(of, tf));
-            });
-
-            return Thread.readMessage(true);
-
-        #end
-
-    } //open
-
-    public static function save(title:String, ?filter:FileFilter) : String {
-        return DialogsLinc.save(title, filter);
-    } //save
-
-    public static function folder(title:String) : String {
-        return DialogsLinc.folder(title);
-    } //folder
-
-} //Dialogs
-
-@:include('linc_dialogs.h')
-private extern class DialogsLinc {
-    
     @:native('linc::dialogs::open')
     static function open(title:String, ?filters:Array<FileFilter>) : String;
 
@@ -51,7 +16,7 @@ private extern class DialogsLinc {
     @:native('linc::dialogs::folder')
     static function folder(title:String) : String;
 
-} //DialogsLinc
+} //Dialogs
 
 
 typedef FileFilter = {
--- linc/linc_dialogs_windows.cpp
+++ linc/linc_dialogs_windows.cpp
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <vector>
 
 #include <windows.h>
 #include <shlobj.h>
